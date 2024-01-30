69830762ae225591866db1e2e160ab07554f670b
Radu Lefter <lefter_radu@yahoo.com>
2024-01-30 18:14:54 +0000

    created functionality to edit task

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,10 +3,14 @@
 
 <head>
   <title>Laravel 10 Task List App</title>
+  @yield('styles')
 </head>
 
 <body>
   <h1>@yield('title')</h1>
+  @if (session()->has('success'))
+  <div>{{session('success')}}</div>
+  @endif
   <div>
     @yield('content')
   </div>
