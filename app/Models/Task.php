8cb1b0975febc38b9c0fb835c005c4e166079769
Radu Lefter <lefter_radu@yahoo.com>
2024-01-30 19:07:02 +0000

    bound model to form requests and implemented mass assignment

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+    //this enables the use of the factory pattern to create model instances for tests.
     use HasFactory;
+
+
+    //defines which attributes (or columns) of the model are mass assignable
+    protected $fillable = ['title', 'description', 'long_description'];
 }
