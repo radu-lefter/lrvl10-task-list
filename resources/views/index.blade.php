c82db993bd2baf9fe18330d01292ffd7cfca8fd7
Radu Lefter <lefter_radu@yahoo.com>
2024-01-31 17:05:43 +0000

    implemented pagination

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,15 +4,15 @@
 
 @section('content')
 @forelse($tasks as $task)
-    <div>
-        <a
-            href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
-    </div>
+<div>
+    <a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
+</div>
 @empty
-    <div>There are no tasks!</div>
+<div>There are no tasks!</div>
 @endforelse
+@if ($tasks->count())
+<nav>
+    {{ $tasks->links() }}
+</nav>
+@endif
 @endsection
-
-@isset($name)
-    <div>The name is: {{ $name }}</div>
-@endisset
