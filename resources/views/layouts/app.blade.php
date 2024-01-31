7e7cda0a164934dd93fa963be155b16f767bb0bb
Radu Lefter <lefter_radu@yahoo.com>
2024-01-31 17:39:13 +0000

    applied general stryled and styled task page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,18 +2,28 @@
 <html>
 
 <head>
-  <title>Laravel 10 Task List App</title>
-  @yield('styles')
+    <title>Laravel 10 Task List App</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <style type="text/tailwindcss">
+        .btn {
+            @apply rounded-md px-2 py-1 text-center font-medium text-slate-700 shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50
+        }
+
+        .link {
+            @apply font-medium text-gray-700 underline decoration-pink-500
+        }
+    </style>
+    @yield('styles')
 </head>
 
-<body>
-  <h1>@yield('title')</h1>
-  @if (session()->has('success'))
-  <div>{{session('success')}}</div>
-  @endif
-  <div>
-    @yield('content')
-  </div>
+<body class="container mx-auto mt-10 mb-10 max-w-lg">
+    <h1 class="mb-4 text-2xl">@yield('title')</h1>
+    @if (session()->has('success'))
+        <div>{{ session('success') }}</div>
+    @endif
+    <div>
+        @yield('content')
+    </div>
 </body>
 
 </html>
